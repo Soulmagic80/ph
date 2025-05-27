43e29e6eae2527d482734d9cdb2e5131fadec544
Philippe La Roche <philippelaroche@me.com>
2025-05-27 16:33:52 +0200

    fix: standardize UI component filenames to lowercase

--- src/components/portfolio/home/PortfolioList.tsx
+++ src/components/portfolio/home/PortfolioList.tsx
@@ -1,5 +1,5 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/Select";
 import { CalendarIcon, TrophyIcon } from "@heroicons/react/24/outline";
-import { Select, SelectItem } from "@tremor/react";
 import { useState } from "react";
 
 export function PortfolioList() {
@@ -9,17 +9,20 @@ export function PortfolioList() {
         <div className="flex flex-col gap-4">
             <div className="flex justify-between items-center">
                 <h1 className="text-2xl font-bold">Portfolios</h1>
-                <Select
-                    value={selectedRanking}
-                    onValueChange={setSelectedRanking}
-                    className="w-[200px]"
-                >
-                    <SelectItem value="current_month" icon={CalendarIcon}>
-                        Current month
-                    </SelectItem>
-                    <SelectItem value="all_time" icon={TrophyIcon}>
-                        All time best
-                    </SelectItem>
+                <Select value={selectedRanking} onValueChange={setSelectedRanking}>
+                    <SelectTrigger className="w-[200px]">
+                        <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="current_month">
+                            <CalendarIcon className="w-4 h-4 mr-2" />
+                            Current month
+                        </SelectItem>
+                        <SelectItem value="all_time">
+                            <TrophyIcon className="w-4 h-4 mr-2" />
+                            All time best
+                        </SelectItem>
+                    </SelectContent>
                 </Select>
             </div>
         </div>
